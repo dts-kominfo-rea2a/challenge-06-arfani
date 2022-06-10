f3b362a2db4f351532806863ba93557fa6a5e1f3
arfan <arfanihidayat@gmail.com>
2022-06-10 16:44:17 +0800

    done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,35 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+
+  
+  fs.readFile(file1, 'utf-8', (err, data) => {
+    data = JSON.parse(data).message.split(" ")[1]
+    let arrData = [data];
+
+    fs.readFile(file2, 'utf-8', (err, data) => {
+      data = JSON.parse(data)[0].message.split(" ")[1]
+      arrData.push(data)
+      
+      fs.readFile(file3, 'utf-8', (err, data) => {
+        data = JSON.parse(data)[0].data.message.split(" ")[1]
+        arrData.push(data)
+  
+        if (!arrData.length >= 2){
+          fnCallback("Data minimal harus 2 kata", null);
+        }
+    
+        fnCallback(null, arrData)
+      });
+    });
+    
+    
+  });
+
+    
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
